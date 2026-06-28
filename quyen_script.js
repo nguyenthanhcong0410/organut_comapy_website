623e4e4667b4bf0446cd80f060503f923b5733bc
nguyenthanhcong041006 <nc5004d@gre.ac.uk>
2026-06-29 03:12:37 +0700

    first commit

---
+++ quyen_script.js
@@ -0,0 +1,107 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+let autoSlideInterval;
+
+
+function updateSliderPosition() {
+    if (slider) {
+        slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+}
+
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        index++;
+        if (index >= slides.length) {
+            index = 0; 
+        }
+        updateSliderPosition();
+    }, 3000);
+}
+
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
+}
+
+
+function nextSlideButton() {
+    index++;
+    if (index >= slides.length) {
+        index = 0;
+    }
+    updateSliderPosition();
+    resetAutoSlide(); 
+}
+
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1; 
+    }
+    updateSliderPosition();
+    resetAutoSlide(); 
+}
+
+
+startAutoSlide();
+
+
+
+function toggleDetails(button) {
+    
+    const extraDetails = button.nextElementSibling;
+    
+    
+    extraDetails.classList.toggle('open');
+    
+    
+    if (extraDetails.classList.contains('open')) {
+        button.textContent = "Hide details";
+    } 
+    
+    else {
+        button.textContent = "More details";
+    }
+}
+
+
+/* --- CHỨC NĂNG 3: HIỆU ỨNG HẠT RƠI BẰNG HÌNH ẢNH THẬT (2 GIÂY) --- */
+window.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('falling-container');
+    if (!container) return;
+
+    
+    const nutImages = [
+        'ảnh sản phẩm/occho.png',
+        'ảnh sản phẩm/hanhnhan.png',
+        'ảnh sản phẩm/hatde.png',
+        'ảnh sản phẩm/yenmach.png',
+        'ảnh sản phẩm/decuoi.png'
+    ]; 
+    const totalNuts = 100; 
+
+    for (let i = 0; i < totalNuts; i++) {
+        const nut = document.createElement('img');
+        nut.classList.add('falling-nut');
+        
+        nut.src = nutImages[Math.floor(Math.random() * nutImages.length)];
+        nut.alt = "nut";
+        
+        nut.style.left = Math.random() * 100 + '%';
+        
+        nut.style.animationDelay = Math.random() * 7 + 's';
+        
+        const randomSize = Math.random() * 20 + 50;
+        nut.style.width = randomSize + 'px';
+        nut.style.height = 'auto';
+
+        container.appendChild(nut);
+    }
+
+    setTimeout(() => {
+        container.remove();
+    }, 5000);
+});
